7bc49330182b67451fae2ddfb5b7c0bb7a4e90bf
Zane Rau <zanemrau@gmail.com>
2022-12-06 00:26:34 -0800

    updated for part 2

--- day01/calorie_counting/calorie_counting.js
+++ day01/calorie_counting/calorie_counting.js
@@ -28,8 +28,14 @@ input_data.split(/\r?\n/).forEach( line => {
   }
 })
 
-// Find the max value
-let elf_largest_calories = Math.max(...calorie_sums);
+// Sort the array in descending order
+calorie_sums.sort(function(a,b) {
+  return b-a;
+});
 
+// Find the max values
+let elf_largest_calories = calorie_sums[0];
+let top_three_sum = calorie_sums[0]+calorie_sums[1]+calorie_sums[2];
 // Report the result
-console.log(`The elf with the largest calorie collection has ${elf_largest_calories} calories!`)
+console.log(`The elf with the largest calorie collection has ${elf_largest_calories} calories!`);
+console.log(`The top 3 elves have ${top_three_sum} calories!`);
